135dfd8530b5394db886832f9afca696bbe3ecfb
JON <jonn.thinkrtc@gmail.com>
2018-10-14 10:35:34 -0400

    added post and comments functions and routes

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
         url(r'^post/new/$',views.CreateView.as_view(),name='post_new'),
         url(r'^post/(?P<pk>\d+)/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
         url(r'^post/(?P<pk>\d+)/remove/$', views.DeleteView.as_view(), name='post_remove'),
+        url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish.as_view(), name='post_publish'),
+        url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish.as_view(), name='post_publish'),
+        url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post.as_view(), name='add_comment_to_post'),
+        url(r'^post/(?P<pk>\d+)/approve/$', views.comment_approve.as_view(), name='comment_approve'),
+        url(r'^post/(?P<pk>\d+)/publish/$', views.comment_remove.as_view(), name='comment_remove'),
+
 
 
 ]
